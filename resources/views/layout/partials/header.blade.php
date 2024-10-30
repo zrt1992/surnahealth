7749953efeb1ad50ec78b8724edc226329270ff9
zrt1992 <zulfiqar.tariq@hotmail.com>
2024-10-30 16:15:59 +0500

    changes in header

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -887,7 +887,7 @@
                 <ul class="main-nav">
                     <li
                         class="has-submenu megamenu {{ Request::is('/', 'index', 'index-2', 'index-3', 'index-4', 'index-5', 'index-6', 'index-7', 'index-8', 'index-9', 'index-10', 'index-11', 'index-12', 'index-13', 'index-14') ? 'active' : '' }}">
-                        <a href="javascript:void(0);">Home
+                        <a href="{{ url('/') }}">Home
 {{--                            <i class="fas fa-chevron-down"></i>--}}
                         </a>
 {{--                        <ul class="submenu mega-submenu">--}}
